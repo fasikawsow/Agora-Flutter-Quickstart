cbf163b9608f1481a491e3ac6c252f97734616b9
Fasikaw Kindye <fasikaw.kindye@sowlab.com>
2022-08-26 06:38:12 +0300

    update

--- lib/src/utils/settings.dart
+++ lib/src/utils/settings.dart
@@ -1,3 +1,3 @@
 /// Define App ID and Token
-const appId = 'Your_App_ID';
-const token = 'Your_Token';
+const appId = 'e418e4243a6c45bebffacb632ae8139a';
+const token = '007eJxTYHi9Me2TS/4Oj7b0HfZbutc9k/Ou+fPP50TRqYpbWXuWxe5XYDAwtjAxMDJOsrBINjVJsTC1TDJPTktLTTG0tEwztDAxdLbnSD44gTP5p6YyEyMDBIL4PAwlqcUl8ckZiXl5qTkMDABk0CYy';
